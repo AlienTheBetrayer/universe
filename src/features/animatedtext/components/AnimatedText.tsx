377a4f0ea3de9540b337d1213414019849337ee0
alienthebetrayer <gpichkurov@gmail.com>
2025-09-21 20:55:56 +0300

    animated heading text done + new unique component

--- src/features/animatedtext/components/AnimatedText.tsx
+++ src/features/animatedtext/components/AnimatedText.tsx
@@ -1,18 +1,26 @@
-import { motion, type HTMLMotionProps } from "motion/react"
+import './AnimatedText.css';
+import { motion } from "motion/react"
 
-interface Props extends HTMLMotionProps<'span'> {
+type Tags = 'h1' | 'h2' | 'h3' | 'h4' | 'span' | 'p';
+
+interface Props {
+    as: Tags;
     children: string;
+    speed?: number;
+    delay?: number;
 }
 
-export const AnimatedText = ({ children, ...rest }: Props) => {
+export const AnimatedText = ({ children, delay, speed, as, ...rest }: Props) => {
+    const MotionTag = motion[as];
+    
     return (
         Array.from(children).map((c, idx) => (
-            <motion.span {...rest} key={`animated-${c}-${idx}`}
-            initial={{ opacity: 0, scale: 2 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ delay: idx / 20, duration: 0.6, type: 'spring', damping: 10, stiffness: 400 }}>
-                {c.toUpperCase()}
-            </motion.span>
+            <MotionTag className='animated-text' {...rest} key={`animated-${c}-${idx}`}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: (delay ?? 0 ) + idx / (20 * (speed ?? 1)), duration: 0.6, type: 'spring', damping: (8 * (speed ?? 1)), stiffness: 400 }}>
+                {c}
+            </MotionTag>
         ))    
     )
 }
--- src/features/pagehome/sections/HeadingSection.tsx
+++ src/features/pagehome/sections/HeadingSection.tsx
@@ -7,15 +7,17 @@ import { AnimatedText } from '../../animatedtext/components/AnimatedText';
 export const HeadingSection = () => {
     return (
         <motion.section
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
         transition={{ delay: 0.5, duration: 1, type: 'spring', stiffness: 200, damping: 200 }}
         className='heading-section'>
             <div className='heading-section-intro'>
-                <AnimatedText className='heading-animated-text'>
+                <AnimatedText as='h1' delay={0.3}>
                     Redefining innovation through personal projects
                 </AnimatedText>
-                <p>Leveraging technology and design to transform personal initiatives into sophisticated, forward-thinking solutions that exemplify creativity, technical excellence, and impactful innovation.</p>
+                <br/>
+                <br/>
+                <AnimatedText as='p' speed={8} delay={2.8}>
+                    Leveraging technology and design to transform personal initiatives into sophisticated, forward-thinking solutions that exemplify creativity, technical excellence, and impactful innovation.
+                </AnimatedText>
             </div>
 
             <div className='heading-grid'>
