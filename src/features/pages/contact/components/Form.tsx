343f762da539bd83049aeb7e189458162b09b803
alienthebetrayer <gpichkurov@gmail.com>
2025-09-29 01:04:34 +0300

    form refactor

--- src/features/pages/contact/components/Form.tsx
+++ src/features/pages/contact/components/Form.tsx
@@ -1,27 +1,30 @@
 import { useState } from 'react';
 import './Form.css';
 
-export const Form = () => {
-    const [name, setName] = useState<string>('');
-    const [email, setEmail] = useState<string>('');
-    const [message, setMessage] = useState<string>('');
+interface FormInputs {
+    name: string;
+    email: string;
+    message: string;
+}
 
+export const Form = () => {
+    const [inputs, setInputs] = useState<FormInputs>({ name: '', email: '', message: '' });
 
     return (
         <form className='contact-form' action='mailto:alienthebusinessman@gmail.com' method='post' encType='text/plain'>
             <div>
-                <input type='text' name='name' required placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
-                <button type='button' onClick={() => setName('')}>✕</button>
+                <input type='text' name='name' required placeholder='Name' value={inputs.name} onChange={(e) => setInputs(prev => ({...prev, name: e.target.value}))} />
+                <button onClick={() => setInputs(prev => ({...prev, name: ''}))}>✕</button>
             </div>
 
             <div>
-                <input type='email' name='name' required placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
-                <button type='button' onClick={() => setEmail('')}>✕</button>
+                <input type='email' name='email' required placeholder='Email' value={inputs.email} onChange={(e) => setInputs(prev => ({...prev, email: e.target.value}))} />
+                <button onClick={() => setInputs(prev => ({...prev, email: ''}))}>✕</button>
             </div>
 
             <div className='contact-form-message-container'>
-                <textarea name='message' required placeholder='Message' value={message} onChange={(e) => setMessage(e.target.value)}/>
-                <button type='button'onClick={() => setMessage('')} >✕</button>
+                <textarea name='message' required placeholder='Message' value={inputs.message} onChange={(e) => setInputs(prev => ({...prev, message: e.target.value}))} />
+                <button onClick={() => setInputs(prev => ({...prev, message: ''}))}>✕</button>
             </div>
 
             <button type='submit'>Send</button>
