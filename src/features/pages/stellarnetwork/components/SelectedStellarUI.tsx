ea90426d029e4848dc760dd9cdb4715818707a2e
alienthebetrayer <gpichkurov@gmail.com>
2025-09-30 19:07:12 +0300

    UI Fully responsive + Animations

--- src/features/pages/stellarnetwork/components/SelectedStellarUI.tsx
+++ src/features/pages/stellarnetwork/components/SelectedStellarUI.tsx
@@ -1,32 +1,80 @@
+import { useMediaQuery } from '../../../../hooks/useMediaQuery';
 import type { Stellar } from '../context/StellarContext';
 import './SelectedStellarUI.css';
 
-import { motion } from 'motion/react';
+import { AnimatePresence, motion } from 'motion/react';
 
 interface Props {
     object: Stellar;
 }
 
 export const SelectedStellarUI = ({ object }: Props) => {
+    const isMobile = useMediaQuery(768);
+
     return (
         <>
             <motion.div className='selected-stellar-ui-left'
-            style={{ y: '-50%' }}
+            style={{ y: isMobile ? '0' : '-50%' }}
             initial={{ x: -300, opacity: 0 }}
             animate={{ x: 0, opacity: 1, transition: { delay: 1, duration: 1.5, ease: 'backOut' }  }}
             exit={{ x: -300, opacity: 0, transition: { duration: 1.5, ease: 'circIn' } }}>
-                <h2>{object.title}</h2>
-                <p>{object.description}</p>
+                <AnimatePresence mode='wait'>
+                    <motion.div className='flex flex-col gap-1'
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    key={object.idx}>
+                        <h2>{object.title}</h2>
+                        <p>{object.description}</p>
+                    </motion.div>
+                </AnimatePresence>
             </motion.div>
-
+            
             <motion.div className='selected-stellar-ui-right'
             style={{ y: '-50%' }}
             initial={{ x: 300, opacity: 0 }}
             animate={{ x: 0, opacity: 1, transition: { delay: 1, duration: 1.5, ease: 'backOut' }  }}
             exit={{ x: 300, opacity: 0, transition: { duration: 1.5, ease: 'circIn' } }}>
-                <h2>{object.title.toUpperCase()}</h2>
-                <p>{object.description.toUpperCase()}</p>
+                <AnimatePresence mode='wait'>
+                    <motion.div className='flex flex-col gap-1'
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    key={object.idx}>
+                        <h2>{object.title.toUpperCase()}</h2>
+                        <p>{object.description.toUpperCase()}</p>
+                    </motion.div>
+                </AnimatePresence>
             </motion.div>
+
+            <motion.div className='selected-stellar-ui-mobile'
+            initial={{ y: 300, opacity: 0 }}
+            animate={{ y: 0, opacity: 1, transition: { delay: 1, duration: 1.5, ease: 'backOut' }  }}
+            exit={{ y: 300, opacity: 0, transition: { duration: 1.5, ease: 'circIn' } }}>
+                <AnimatePresence mode='wait'>
+                    <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    key={object.idx}>
+                        <h2>{object.title}</h2>
+                        <p>{object.description}</p>
+                    </motion.div>
+                </AnimatePresence>
+
+                <AnimatePresence mode='wait'>
+                    <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    key={`${object.idx}-2`}>
+                        <h2>{object.title.toUpperCase()}</h2>
+                        <p>{object.description.toUpperCase()}</p>
+                    </motion.div>
+                </AnimatePresence>
+
+            </motion.div>
+            
         </>
     )
 }
