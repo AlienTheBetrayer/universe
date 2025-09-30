7935f951752ec2a3bcb6c064fb94fe7a1fd6bd1f
alienthebetrayer <gpichkurov@gmail.com>
2025-09-30 21:17:22 +0300

    Fully revamped responsiveness

--- src/features/pages/stellarnetwork/components/SelectedStellarUI.tsx
+++ src/features/pages/stellarnetwork/components/SelectedStellarUI.tsx
@@ -19,70 +19,41 @@ export const SelectedStellarUI = ({ object }: Props) => {
             animate={{ x: 0, opacity: 1, transition: { delay: 1, duration: 1.5, ease: 'backOut' }  }}
             exit={{ x: -300, opacity: 0, transition: { duration: 1.5, ease: 'circIn' } }}>
                 <AnimatePresence mode='wait'>
-                    <motion.div className='flex flex-col gap-1'
+                    <motion.div
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                     key={object.idx}>
-                        <h2>{object.content.first.title}</h2>
-                        { object.content.first.description.map((p, idx) => (
-                            <p key={`${object.idx}-${idx}`}>{p}</p>
-                        ))}
+                        <h3>{object.content.first.title}</h3>
+                        <div>
+                            { object.content.first.description.map((p, idx) => (
+                                <p key={`${object.idx}-${idx}`}>{p}</p>
+                            ))}
+                        </div>
                     </motion.div>
                 </AnimatePresence>
             </motion.div>
             
             <motion.div className='selected-stellar-ui-right'
-            style={{ y: '-50%' }}
+            style={{ y: isMobile ? '0' : '-50%' }}
             initial={{ x: 300, opacity: 0 }}
             animate={{ x: 0, opacity: 1, transition: { delay: 1, duration: 1.5, ease: 'backOut' }  }}
             exit={{ x: 300, opacity: 0, transition: { duration: 1.5, ease: 'circIn' } }}>
                 <AnimatePresence mode='wait'>
-                    <motion.div className='flex flex-col gap-1'
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    key={object.idx}>
-                        <h2>{object.content.second.title}</h2>
-                        { object.content.second.description.map((p, idx) => (
-                            <p key={`${object.idx}-${idx}`}>{p}</p>
-                        ))}
-                    </motion.div>
-                </AnimatePresence>
-            </motion.div>
-
-            <motion.div className='selected-stellar-ui-mobile'
-            initial={{ y: -300, opacity: 0 }}
-            animate={{ y: 0, opacity: 1, transition: { delay: 1, duration: 1.5, ease: 'backOut' }  }}
-            exit={{ y: -300, opacity: 0, transition: { duration: 1.5, ease: 'circIn' } }}>
-                <AnimatePresence mode='wait'>
                     <motion.div
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                     key={object.idx}>
-                        <h2>{object.content.first.title}</h2>
-                        { object.content.first.description.map((p, idx) => (
-                            <p key={`${object.idx}-${idx}`}>{p}</p>
-                        ))}
+                        <h3>{object.content.second.title}</h3>
+                        <div>
+                            { object.content.second.description.map((p, idx) => (
+                                <p key={`${object.idx}-${idx}`}>• {p}</p>
+                            ))}
+                        </div>
                     </motion.div>
                 </AnimatePresence>
-
-                <AnimatePresence mode='wait'>
-                    <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    key={`${object.idx}-2`}>
-                        <h2>{object.content.second.title}</h2>
-                        { object.content.second.description.map((p, idx) => (
-                            <p key={`${object.idx}-${idx}`}>{p}</p>
-                        ))}
-                    </motion.div>
-                </AnimatePresence>
-
             </motion.div>
-            
         </>
     )
 }
