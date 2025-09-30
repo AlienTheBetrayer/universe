368a8727de5dc31165442bb79f67340abad0729e
alienthebetrayer <gpichkurov@gmail.com>
2025-09-30 21:29:31 +0300

    Camera revamp

--- src/features/pages/stellarnetwork/hooks/useStellarCamera.ts
+++ src/features/pages/stellarnetwork/hooks/useStellarCamera.ts
@@ -1,15 +1,38 @@
 import gsap from "gsap";
-import { useEffect } from "react"
+import { useRef } from "react"
 import { useStellarContext } from "../context/StellarContext";
-import type { Camera } from "@react-three/fiber";
+import { useFrame, type Camera } from "@react-three/fiber";
 import { useMediaQuery } from "../../../../hooks/useMediaQuery";
 
 export const useStellarCamera = (camera: Camera) => {
-    const [state, ] = useStellarContext();
     const isMobile = useMediaQuery(768);
+    const [state, ] = useStellarContext();
+    const cameraTween = useRef<GSAPTween | null>(null);
 
-    useEffect(() => {
-        const position = state.selected !== -1 ? [state.stellars[state.selected].x, state.stellars[state.selected].y, isMobile ? 0.175 : 0.12] : [0, 0, 5];
-        gsap.to(camera.position, { x: position[0], y: position[1], z: position[2], duration: 2, ease: 'circ.inOut' })
-    }, [state.selected]);
+    useFrame(() => {
+        if(state.selected !== -1) {
+            const target = state.stellars[state.selected];
+            if(!target)
+                return;
+
+            cameraTween.current?.kill();
+
+            cameraTween.current = gsap.to(camera.position, {
+                x: target.x,
+                y: target.y,
+                z: isMobile ? 0.175 : 0.12, 
+                duration: 2,
+                ease: "circ.out",
+            });
+        } else {
+            cameraTween.current?.kill();
+            cameraTween.current = gsap.to(camera.position, {
+                x: 0,
+                y: 0,
+                z: 5,
+                duration: 2,
+                ease: "circ.out",
+            });
+        }
+    });
 }
