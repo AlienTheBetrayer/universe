f210edea0678d2dd9fe23fb60ed5c77c2d8ec28c
alienthebetrayer <gpichkurov@gmail.com>
2025-10-06 16:58:59 +0300

    Animations

--- src/features/horizontalcatalogue/components/HorizontalCatalogueItem.tsx
+++ src/features/horizontalcatalogue/components/HorizontalCatalogueItem.tsx
@@ -1,7 +1,7 @@
 import { usePopup } from '../../../hooks/usePopup';
 import type { CatalogueItem } from './HorizontalCatalogue';
 import './HorizontalCatalogueItem.css';
-import { AnimatePresence, motion } from 'motion/react';
+import { motion } from 'motion/react';
 import { HorizontalOrderPopup } from './HorizontalOrderPopup';
 
 interface Props {
@@ -20,18 +20,17 @@ export const HorizontalCatalogueItem = ({ item, onPurchase }: Props) => {
 
     return (
         <>
-            <AnimatePresence>
-                <motion.div
-                    key={item.title}
-                    className='horizontal-catalogue-item'
-                    animate={{ display: item.visible ? 'flex' : 'none' }}
-                    exit={{ opacity: 0 }}>
-                        <h3>{item.title}</h3>
-                        <p>{item.content}</p>
-                        <span className='horizontal-catalogue-item-idx'>{item.idx}</span>
-                        <button onClick={() => handleOrder()} className='horizontal-catalogue-item-order-button'>Order</button>
-                </motion.div>
-            </AnimatePresence>
+            <motion.div
+                layout
+                key={item.title}
+                className='horizontal-catalogue-item'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: item.visible ? 1 : 0, display: item.visible ? 'flex' : 'none' }}>
+                    <h3>{item.title}</h3>
+                    <p>{item.content}</p>
+                    <span className='horizontal-catalogue-item-idx'>{item.idx + 1}</span>
+                    <button onClick={() => handleOrder()} className='horizontal-catalogue-item-order-button'>Order</button>
+            </motion.div>
 
             { popup.render() }
         </>
