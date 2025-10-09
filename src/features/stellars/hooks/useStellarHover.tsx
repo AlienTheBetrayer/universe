ca2857651634544358ea8bf99f7c6b504b3a3874
alienthebetrayer <gpichkurov@gmail.com>
2025-10-09 23:14:24 +0300

    Hover popup is dynamic

--- src/features/stellars/hooks/useStellarHover.tsx
+++ src/features/stellars/hooks/useStellarHover.tsx
@@ -2,30 +2,33 @@ import { useEffect, useRef } from "react"
 import { useStellarContext } from "../context/StellarContext";
 import { usePopup } from "../../../hooks/usePopup";
 import { StellarHoverPopup } from "../components/StellarHoverPopup";
-import { useCursorRef } from "../../../hooks/useCursorRef";
 
 export const useStellarHover = () => {
     const [state,] = useStellarContext();
+
+    // popup handling
+    const ref = useRef<HTMLDivElement>(null);
+    const popup = usePopup(<StellarHoverPopup stellar={state.stellars[state.hovered as number]} ref={ref}/>, false);
 
     useEffect(() => {
         document.body.style.cursor = state.hovered === false ? 'auto' : 'pointer';
         popup.setShown(state.hovered !== false);
     }, [state.hovered]);
 
-    // popup handling
-    const cursor = useCursorRef();
-    const ref = useRef<HTMLDivElement>(null);
-    const popup = usePopup(<StellarHoverPopup stellar={state.stellars[state.hovered as number]} ref={ref}/>, false);
-
     useEffect(() => {
-        if(ref.current && popup.shown && state.selected === false) {
-            ref.current.style.display = 'flex';
-            ref.current.style.left = `${cursor.current.x}px`;
-            ref.current.style.top = `${cursor.current.y + window.scrollY}px`;
+        const handle = (e: PointerEvent) => {
+            if(ref.current && popup.shown && state.selected === false) {
+                ref.current.style.display = 'flex';
+                ref.current.style.left = `${e.clientX}px`;
+                ref.current.style.top = `${e.clientY + window.scrollY}px`;
+            }
         }
 
         if(state.selected !== false)
             popup.setShown(false);
+
+        window.addEventListener('pointermove', handle);
+        return () => window.removeEventListener('pointermove', handle);
     }, [popup.shown, state.selected]);
 
     return { render: popup.render };
