d9b1de4c3639f2161335be593e6ea5d35aa3d69f
alienthebetrayer <gpichkurov@gmail.com>
2025-10-17 16:53:13 +0300

    Staggered background theme color + animation done

--- src/features/staggeredbackground/components/StaggeredBackground.tsx
+++ src/features/staggeredbackground/components/StaggeredBackground.tsx
@@ -3,7 +3,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from 'gsap';
 import './StaggeredBackground.css';
 
-const ElementSize = 48;
+const ElementSize = 128;
 
 export const StaggeredBackground = () => {
     const containerRef = useRef<HTMLDivElement>(null);
@@ -26,13 +26,40 @@ export const StaggeredBackground = () => {
     }, []);
 
     useGSAP(() => {
-        // gsap.to('.staggered-background-element', {
-        //     scale: 2,
-        //     stagger: {
-        //         each: 0.1,
-        //         from: 'center'
-        //     }
-        // });
+        const timeline = gsap.timeline();
+
+        timeline.fromTo('.staggered-background-element', 
+            {
+                scale: 0,
+                y: () => gsap.utils.random(-300, 300),
+                x: () => gsap.utils.random(-300, 300),
+                borderRadius: 48,
+                rotate: () => gsap.utils.random(0, 360),
+                opacity: 0.5,
+            },
+            {
+                delay: 2,
+                duration: 1,
+                x: 0,
+                y: 0,
+                rotate: () => gsap.utils.random(0, 360),
+                scale: 0.8,
+                opacity: 0.05,
+                stagger: {
+                    each: 0.15,
+                    from: 'start'
+                }
+        }).to('.staggered-background-element', {
+            duration: 4,
+            rotate: 0,
+            scale: 1,
+            borderRadius: 8,
+            ease: 'back.inOut',
+            stagger: {
+                each: 0.15,
+                from: 'start'
+            }
+        }, '3');
     }, { scope: containerRef, dependencies: [rows, columns] });
     
 
