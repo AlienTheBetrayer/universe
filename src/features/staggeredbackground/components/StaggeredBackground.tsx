ca201f769e7bff091e268320c74fd4779063c910
alienthebetrayer <gpichkurov@gmail.com>
2025-10-17 15:17:50 +0300

    Grid done (no animation yet)

--- src/features/staggeredbackground/components/StaggeredBackground.tsx
+++ src/features/staggeredbackground/components/StaggeredBackground.tsx
@@ -1,9 +1,53 @@
+import { useEffect, useRef, useState } from 'react';
+import { useGSAP } from "@gsap/react";
+import gsap from 'gsap';
 import './StaggeredBackground.css';
 
-export const StaggeredBackground = () => {
-    return (
-        <div className='staggered-background'>
+const ElementSize = 48;
 
+export const StaggeredBackground = () => {
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const [columns, setColumns] = useState<number>(0);
+    const [rows, setRows] = useState<number>(0);
+
+    useEffect(() => {
+        const handle = () => {
+            if(containerRef.current) {
+                const containerRect = containerRef.current.getBoundingClientRect();
+                setColumns(containerRect.width / ElementSize);
+                setRows(containerRect.height / ElementSize);
+            }
+        }
+        handle();
+
+        window.addEventListener('resize', handle);
+        return () => window.removeEventListener('resize', handle);
+    }, []);
+
+    useGSAP(() => {
+        // gsap.to('.staggered-background-element', {
+        //     scale: 2,
+        //     stagger: {
+        //         each: 0.1,
+        //         from: 'center'
+        //     }
+        // });
+    }, { scope: containerRef, dependencies: [rows, columns] });
+    
+
+    return (
+        <div 
+        className='staggered-background'
+        ref={containerRef}>
+            { Array.from({ length: rows * columns }).map((_, idx) => (
+                <div 
+                style={{ width: `${ElementSize}px`, height: `${ElementSize}px`}}
+                className='staggered-background-element'
+                key={idx}>
+
+                </div>
+            ))}
         </div>
     )
 }
