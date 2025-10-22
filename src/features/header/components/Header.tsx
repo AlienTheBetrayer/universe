e809681b8669d3a728e7abef86dcde15a27bdad8
alienthebetrayer <gpichkurov@gmail.com>
2025-10-23 01:55:38 +0300

    Header small recomposition

--- src/features/header/components/Header.tsx
+++ src/features/header/components/Header.tsx
@@ -1,22 +1,22 @@
 import './Header.css';
 
 import { motion } from 'motion/react';
-import { UniversalLink } from '../../ui/UniversalLink/components/UniversalLink';
-import { LinkButton } from '../../ui/LinkButton/components/LinkButton';
 import { useMediaQuery } from '../../../hooks/useMediaQuery';
-import { Button } from '../../ui/Button/components/Button';
 import { usePopup } from '../../../hooks/usePopup';
 import { HeaderMenu } from '../../headermenu/components/HeaderMenu';
+import { Button } from '../../ui/Button/components/Button';
+import { LinkButton } from '../../ui/LinkButton/components/LinkButton';
+import { UniversalLink } from '../../ui/UniversalLink/components/UniversalLink';
 import { useHeaderAnimation } from '../hooks/useHeaderAnimation';
 
+import forkImg from '../assets/fork.svg';
 import launchImg from '../assets/launch.svg';
 import starImg from '../assets/star.svg';
 import stellarImg from '../assets/stellar.svg';
-import forkImg from '../assets/fork.svg';
 
 import { useLocalStore } from '../../../zustand/localStore';
-import { ToggleButton } from '../../ui/ToggleButton/ToggleButton';
 import { useTooltips } from '../../tooltip/hooks/useTooltips';
+import { ToggleButton } from '../../ui/ToggleButton/ToggleButton';
 
 export const Header = () => {
     const isMobile = useMediaQuery(768);
@@ -24,7 +24,7 @@ export const Header = () => {
     const { theme, toggleTheme } = useLocalStore();
 
     const headerMenuPopup = usePopup(
-        <HeaderMenu onInteract={() => headerMenuPopup.setShown(false)} />,
+        <HeaderMenu onInteract={() => headerMenuPopup.setShown(false)} />
     );
     const tooltips = useTooltips();
 
@@ -60,83 +60,89 @@ export const Header = () => {
                 </UniversalLink>
 
                 {/* display none on mobile */}
-                <motion.div
-                    className='flex align-center h-full'
-                    style={{ display: !isMobile ? 'flex' : 'none' }}
-                    layout
-                    initial={{ gap: loaded.header ? '1rem' : '0.1rem' }}
-                    animate={{
-                        gap: '1rem',
-                        transition: {
-                            delay: 6,
+                {!isMobile ? (
+                    <motion.div
+                        className='flex align-center h-full'
+                        style={{ display: !isMobile ? 'flex' : 'none' }}
+                        layout
+                        initial={{ gap: loaded.header ? '1rem' : '0.1rem' }}
+                        animate={{
+                            gap: '1rem',
+                            transition: {
+                                delay: 6,
+                                type: 'spring',
+                                duration: 0.6,
+                                stiffness: 200,
+                                damping: 50,
+                            },
+                        }}
+                        transition={{
                             type: 'spring',
+                            delay: 1,
                             duration: 0.6,
                             stiffness: 200,
                             damping: 50,
-                        },
-                    }}
-                    transition={{
-                        type: 'spring',
-                        delay: 1,
-                        duration: 0.6,
-                        stiffness: 200,
-                        damping: 50,
-                    }}
-                >
-                    <LinkButton
-                        className='header-big-button header-github-button'
-                        to='/github'
+                        }}
                     >
-                        <img src={forkImg} alt='' />
-                        Github Emulation
-                    </LinkButton>
+                        <LinkButton
+                            className='header-big-button header-github-button'
+                            to='/github'
+                        >
+                            <img src={forkImg} alt='' />
+                            Github Emulation
+                        </LinkButton>
 
-                    <LinkButton
-                        className='header-big-button header-stellar-button'
-                        to='/stellarnetwork'
-                    >
-                        <img src={stellarImg} alt='' />
-                        Stellar Network
-                    </LinkButton>
+                        <LinkButton
+                            className='header-big-button header-stellar-button'
+                            to='/stellarnetwork'
+                        >
+                            <img src={stellarImg} alt='' />
+                            Stellar Network
+                        </LinkButton>
 
-                    <LinkButton
-                        className='header-big-button header-forge-button'
-                        to='/forge'
-                    >
-                        <img src={launchImg} alt='' />
-                        Forge
-                    </LinkButton>
+                        <LinkButton
+                            className='header-big-button header-forge-button'
+                            to='/forge'
+                        >
+                            <img src={launchImg} alt='' />
+                            Forge
+                        </LinkButton>
 
-                    <ToggleButton
-                        ref={(el) =>
-                            tooltips.set(
-                                0,
-                                `${theme === 'dark' ? '<mark>Dark</mark> / Light' : 'Dark / <mark>Light</mark>'} theme`,
-                                el,
-                                'down',
-                                24,
-                            )
+                        <ToggleButton
+                            ref={(el) =>
+                                tooltips.set(
+                                    0,
+                                    `${
+                                        theme === 'dark'
+                                            ? '<mark>Dark</mark> / Light'
+                                            : 'Dark / <mark>Light</mark>'
+                                    } theme`,
+                                    el,
+                                    'down',
+                                    24
+                                )
+                            }
+                            value={theme === 'light'}
+                            onToggled={() => toggleTheme()}
+                        />
+                    </motion.div>
+                ) : (
+                    <Button
+                        layout
+                        transition={{
+                            delay: 1,
+                            type: 'spring',
+                            stiffness: 150,
+                            damping: 50,
+                        }}
+                        className='header-open-menu'
+                        onClick={() =>
+                            headerMenuPopup.setShown((prev) => !prev)
                         }
-                        value={theme === 'light'}
-                        onToggled={() => toggleTheme()}
-                    />
-                </motion.div>
-
-                {/* mobile menu open button (display none not on mobile) */}
-                <Button
-                    layout
-                    transition={{
-                        delay: 1,
-                        type: 'spring',
-                        stiffness: 150,
-                        damping: 50,
-                    }}
-                    style={{ display: isMobile ? 'flex' : 'none' }}
-                    className='header-open-menu'
-                    onClick={() => headerMenuPopup.setShown((prev) => !prev)}
-                >
-                    Menu
-                </Button>
+                    >
+                        Menu
+                    </Button>
+                )}
             </motion.nav>
 
             {headerMenuPopup.render()}
