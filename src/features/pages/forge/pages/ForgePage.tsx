4ef25e74288d81b49bbe233f46e1670704769839
alienthebetrayer <gpichkurov@gmail.com>
2025-10-22 19:46:19 +0300

    Forge show-up animation

--- src/features/pages/forge/pages/ForgePage.tsx
+++ src/features/pages/forge/pages/ForgePage.tsx
@@ -2,12 +2,17 @@ import { Forge } from '../../../forge/components/Forge';
 import { Page } from '../../../layout/components/Page';
 import './ForgePage.css';
 
+import { motion } from 'motion/react';
+
 export const ForgePage = () => {
     return (
         <Page>
-            <section className='forge-section container'>
+            <motion.section className='forge-section container'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1}}
+            transition={{ delay: 1, duration: 1 }}>
                 <Forge />
-            </section>
+            </motion.section>
         </Page>
     );
 };
