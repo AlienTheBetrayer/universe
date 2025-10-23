e9db129b8770231b890bc02e7b474c0c6da6ee19
alienthebetrayer <gpichkurov@gmail.com>
2025-10-23 15:20:09 +0300

    Drag implemented + progress on the button

--- src/features/forge/components/selectcards/ForgeSelectCard.tsx
+++ src/features/forge/components/selectcards/ForgeSelectCard.tsx
@@ -1,4 +1,5 @@
-import { forwardRef } from 'react';
+import gsap from 'gsap';
+import { forwardRef, useEffect, useRef, useState } from 'react';
 import { Button } from '../../../ui/Button/components/Button';
 import './ForgeSelectCard.css';
 
@@ -21,9 +22,41 @@ interface Props {
 
 export const ForgeSelectCard = forwardRef<HTMLButtonElement, Props>(
     ({ title, description, type }, ref) => {
+        const [selected, setSelected] = useState<boolean>(false);
+        const progressRef = useRef<HTMLDivElement>(null);
+        const tweenRef = useRef<gsap.core.Tween>(null);
+        const [isDraggable, setIsDraggable] = useState<boolean>(false);
+
+        useEffect(() => {
+            tweenRef.current = gsap.to(progressRef.current, {
+                scaleX: selected ? 1 : 0,
+                duration: 1,
+                onComplete: () => {
+                    setIsDraggable(selected);
+                },
+            });
+
+            return () => {
+                tweenRef.current?.kill();
+            };
+        }, [selected]);
+
         return (
-            <Button className='forge-select-card' ref={ref}>
-                {title}
+            <Button
+                drag={isDraggable}
+                transition={{ duration: 0 }}
+                className='forge-select-card'
+                ref={ref}
+                onPointerDown={() => setSelected(true)}
+                onPointerUp={() => setSelected(false)}
+            >
+                <span style={{ zIndex: 1 }}>{title}</span>
+
+                <div
+                    style={{ zIndex: 0 }}
+                    ref={progressRef}
+                    className='forge-select-card-progress'
+                ></div>
             </Button>
         );
     }
