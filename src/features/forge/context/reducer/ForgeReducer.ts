b1dba253c4e5b5a6b31c45cb7990d47265b9df1e
alienthebetrayer <gpichkurov@gmail.com>
2025-10-24 00:58:23 +0300

    Reducer loop optimization

--- src/features/forge/context/reducer/ForgeReducer.ts
+++ src/features/forge/context/reducer/ForgeReducer.ts
@@ -21,26 +21,30 @@ export const ForgeReducer = (
             };
         case 'SET_EFFECT_SLOT': {
             const copy = new Map<number, ForgeCardType>(state.effectSlots);
-            copy.forEach((val, key) => {
+            for (const [key, val] of copy) {
                 if (val === action.cardType) {
                     copy.delete(key);
+                    break;
                 }
-            });
+            }
             copy.set(action.idx, action.cardType);
+
             return { ...state, effectSlots: copy };
         }
         case 'REMOVE_EFFECT_SLOT': {
             const copy = new Map<number, ForgeCardType>(state.effectSlots);
-            copy.forEach((val, key) => {
+            for (const [key, val] of copy) {
                 if (val === action.cardType) {
                     copy.delete(key);
+                    break;
                 }
-            });
+            }
+
             return { ...state, effectSlots: copy };
         }
         case 'CANCEL_CURRENT':
             return { ...state, awaitingCancel: state.dragging.idx };
         case 'RESTORE_CANCEL':
-            return { ...state, awaitingCancel: false }
+            return { ...state, awaitingCancel: false };
     }
 };
