d26ffef102bd741e47bf36ddb47aa1b0d0f971cf
alienthebetrayer <gpichkurov@gmail.com>
2025-10-27 15:40:41 +0200

    World context

---
+++ src/features/forge/context/WorldContext.tsx
@@ -0,0 +1,38 @@
+import React, { createContext, useContext, useState } from 'react';
+
+// types
+export interface WorldData {
+    autoRotationEnabled: boolean;
+}
+
+export type WorldContextType = [
+    WorldData,
+    React.Dispatch<React.SetStateAction<WorldData>>
+];
+
+// context
+export const WorldContext = createContext<WorldContextType | null>(null);
+
+// provider
+interface Props {
+    children?: React.ReactNode;
+}
+
+export const WorldProvider = ({ children }: Props) => {
+    const [state, setState] = useState<WorldData>({
+        autoRotationEnabled: true,
+    });
+
+    return (
+        <WorldContext.Provider value={[state, setState]}>
+            {children}
+        </WorldContext.Provider>
+    );
+};
+
+// hooks
+export const useWorldContext = () => {
+    const ctx = useContext(WorldContext);
+    if (!ctx) throw new Error('useWorldContext() is used incorrectly.');
+    return ctx;
+};
