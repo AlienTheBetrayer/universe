114c178f614bf209be25b201500aff4d6dd864cc
alienthebetrayer <gpichkurov@gmail.com>
2025-10-29 00:34:16 +0200

    onSelect() callback in <SelectorMenu/>

--- src/features/ui/SelectorMenu/components/SelectorMenu.tsx
+++ src/features/ui/SelectorMenu/components/SelectorMenu.tsx
@@ -6,21 +6,28 @@ import { Button } from '../../Button/components/Button';
 
 interface Props {
     items: string[];
+    onSelect?: (item: string) => void;
 }
 
-export const SelectorMenu = ({ items }: Props) => {
+export const SelectorMenu = ({ items, onSelect }: Props) => {
     const [selectedIdx, setSelectedIdx] = useState<number>(0);
 
     return (
         <ul className='selector-menu'>
             {items.map((item, idx) => (
                 <li className='selector-menu-item' key={idx}>
-                    <Button onClick={() => setSelectedIdx(idx)}>{item}</Button>
+                    <Button
+                        onClick={() => {
+                            setSelectedIdx(idx);
+                            onSelect?.(item);
+                        }}
+                    >
+                        {item}
+                    </Button>
                     {selectedIdx === idx && (
                         <motion.div
                             className='selector-menu-item-underline'
                             layoutId='underline'
-   
                         />
                     )}
                 </li>
