8e3db8c16c9d6b90848426d61ef9e2a22153dc69
alienthebetrayer <gpichkurov@gmail.com>
2025-10-29 20:00:34 +0200

    Bonus effect done (no logic yet)

--- src/features/forge/components/effects/components/ForgeBonusEffect.tsx
+++ src/features/forge/components/effects/components/ForgeBonusEffect.tsx
@@ -1,9 +1,25 @@
+import { useForgeContext } from '../../../context/ForgeContext';
 import './ForgeBonusEffect.css';
 
-export const ForgeBonusEffect = () => {
-    return (
-        <div className='forge-bonus-effect'>
+import { motion } from 'motion/react';
 
+export const ForgeBonusEffect = () => {
+    const [state, dispatch] = useForgeContext();
+
+    return (
+        <div
+            className='forge-bonus-effect'
+            style={{ borderColor: `hsl(224, ${state.effectSlots.length * 10}%, 50%)` }}
+        >
+            <motion.span
+                className='forge-bonus-effect-not-ready'
+                key={state.effectSlots.length}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                style={{ color: `hsl(224, ${state.effectSlots.length * 10}%, 50%)` }}
+            >
+                {state.effectSlots.length} / 9
+            </motion.span>
         </div>
-    )
-}
+    );
+};
