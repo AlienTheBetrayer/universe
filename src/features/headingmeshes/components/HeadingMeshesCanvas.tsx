af2e8dbf034474879bf6c455d440f523f00f66f7
alienthebetrayer <gpichkurov@gmail.com>
2025-11-12 13:41:19 +0200

    Update HeadingMeshesCanvas.tsx

--- src/features/headingmeshes/components/HeadingMeshesCanvas.tsx
+++ src/features/headingmeshes/components/HeadingMeshesCanvas.tsx
@@ -20,6 +20,7 @@ export const HeadingMeshesCanvas = React.memo(({ progress }: Props) => {
     const isMobile = useMediaQuery(640);
     const performanceTimeout = useRef<number | false>(false);
     const [isLagging, setIsLagging] = useState<boolean>(false);
+    const [isLaggingDisabled, setIsLaggingDisabled] = useState<boolean>(false);
 
     return (
         <motion.div
@@ -37,28 +38,34 @@ export const HeadingMeshesCanvas = React.memo(({ progress }: Props) => {
                 <span>DESTINY</span>
             </motion.div>
 
-            {theme === 'dark' && isLagging && (
-                <motion.div
-                    className='interactive-particles-fps-warning'
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                >
-                    <p>
-                        <small>
-                            Effects <u>reduced!</u>
-                        </small>
-                    </p>
-                </motion.div>
-            )}
+            {theme === 'dark' &&
+                isLagging &&
+                !isMobile &&
+                !isLaggingDisabled && (
+                    <motion.button
+                        className='interactive-particles-fps-warning'
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        onClick={() => setIsLaggingDisabled(true)}
+                    >
+                        <p>
+                            <small>
+                                Effects <u>reduced!</u>
+                            </small>
+                        </p>
+                    </motion.button>
+                )}
 
             <Canvas style={{ width: '100%', height: '100%' }}>
                 <pointLight position={[0, 0, 0]} intensity={24} />
 
-                {theme === 'dark' && !isMobile && !isLagging && (
-                    <EffectComposer>
-                        <Bloom emissiveThreshold={0} />
-                    </EffectComposer>
-                )}
+                {theme === 'dark' &&
+                    !isMobile &&
+                    (!isLagging || isLaggingDisabled) && (
+                        <EffectComposer>
+                            <Bloom emissiveThreshold={0} />
+                        </EffectComposer>
+                    )}
 
                 <Center>
                     <HeadingMeshes
